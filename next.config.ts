913e1a1168b867a8e9c67e648cf4a806826c9aca
Loïc <loiczeiss@gmail.com>
2025-01-16 17:23:46 +0100

    eslint ignored during prod

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,10 @@ const nextConfig: NextConfig = {
           hostname: 'res.cloudinary.com',
         },
       ],
+    },eslint: {
+      // Warning: This allows production builds to successfully complete even if
+      // your project has ESLint errors.
+      ignoreDuringBuilds: true,
     },
   
 };
