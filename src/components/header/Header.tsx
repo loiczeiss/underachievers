b3a03d153e065d9eab96230e6eadf81ff216d3b6
Loïc <loiczeiss@gmail.com>
2025-03-13 17:01:12 +0100

    fixing font size title

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -8,7 +8,7 @@ export default function Header() {
   return (
     <>
       <header className="flex flex-row h-28 backdrop-blur-sm shadow-md">
-        <section className="w-1/2 px-8 py-4 flex items-center">
+        <section className="lg:w-1/2 px-8 py-4 flex items-center">
           <Button className="text-xs  md:text-base lg:text-3xl p-0 bg-transparent hover:text-gray-300 " onPress={()=>redirect(paths.home())}>UnderAchievers</Button>
         </section>
         <section className="w-1/2 flex items-center justify-end px-8">
