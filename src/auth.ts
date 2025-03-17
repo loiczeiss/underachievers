a28bf06ad982a6b0811280e2187b58d41118de83
Loïc <loiczeiss@gmail.com>
2025-03-17 15:28:01 +0100

    corrected auth adapter errors

--- src/auth.ts
+++ src/auth.ts
@@ -3,17 +3,20 @@ import Google from "next-auth/providers/google"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { db } from "./db"
 
+import authConfig from "./auth.config"
 
 
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(db),
-  providers: [Google],
-  secret: process.env.AUTH_SECRET,
+
+  secret: process.env.AUTH_SECRET, session: { strategy: "jwt" },
+  ...authConfig,
 
   callbacks: {
     async session({ session }) {
       // Map session fields here
+
       return session;
     },
     async signIn({ user }) {
